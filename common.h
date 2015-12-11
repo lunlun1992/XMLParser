5b9e08d21aac7b9c42c0327fa70af12ab204ad99
WenyuLei <leiwenyu@pku.edu.cn>
2015-12-11 14:56:03 +0800

    Update common.h
    add error value

--- common.h
+++ common.h
@@ -11,7 +11,7 @@
 #define DATA_SET_MAX 10240
 #define MAX_COUNT_EVENTS 10000
 #define MAX_COUNT_DATA_SETS 100000
-
+#define Event_Stream_Size 1024
 typedef enum XMLEventLabel_t
 {
 	STAG = 1,
@@ -21,11 +21,26 @@ typedef enum XMLEventLabel_t
 	CDATA = 5
 }XMLEventLabel;
 
+typedef enum ErrorValue_t
+{
+	NoError = 0,
+	MissElem = 1,    //miss <,\,.......
+	NameStartCharError = 2,
+	NameCharError = 3,
+	AttributeError = 4,
+	CommentStartError = 5,
+	CommentError = 6,
+	CharError = 7,
+	PITargetError = 8,
+	PIError = 9,
+}ErrorValue;
+
 typedef struct XMLEvents_t
 {
 	XMLEventLabel i_label;
 	char *p_event_start;
 	int64_t i_event_length;
+
 	char *event_stream;
 	int64_t i_event_stream_length;
 }XMLEvents;
